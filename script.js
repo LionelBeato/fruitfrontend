b4934de589e5d519f280988e7122b81871995705
LionelBeato <lionel.beato@gmail.com>
2020-02-11 16:02:11 -0500

    adds graphql query to frontend

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const myFunc = () => {
-    fetch('https://rocky-forest-78029.herokuapp.com/data', {
-    })
-        .then(response => response.json())
-        // .then(json => console.log(json))
-        .then(json => document.getElementById("apples").innerHTML = `there are ${json.amount} ${json.name}`)
-}
+    // fetch('https://rocky-forest-78029.herokuapp.com/data', {
+    // })
+    //     .then(response => response.json())
+    //     // .then(json => console.log(json))
+    //     .then(json => document.getElementById("apples").innerHTML = `there are ${json.amount} ${json.name}`)
+
+
+
+    fetch('/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({query: "{ hello }"})
+      })
+        .then(r => r.json())
+        .then(data => console.log('data returned:', data));
+    }
+
